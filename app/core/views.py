5a2db6124fadfa818b7da0b1ab7743306923410f
Renato Costa Ribas <ribascr@gmail.com>
2026-01-08 22:19:26 -0300

    Add student dashboard and attendance registration views

--- app/core/views.py
+++ app/core/views.py
@@ -45,3 +45,34 @@ class RegistrarPresencaViewSet(viewsets.ViewSet):
             duracao_minutos=duracao,
         )
         return Response(PresencaSerializer(presenca).data, status=status.HTTP_201_CREATED)
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Aluno, Presenca, ConfiguracaoDojo
+from django.utils.timezone import now
+
+def aluno_dashboard(request, aluno_id):
+    aluno = get_object_or_404(Aluno, id=aluno_id)
+    presencas = aluno.presencas.order_by("-data_aula")[:10]
+
+    return render(request, "core/aluno_dashboard.html", {
+        "aluno": aluno,
+        "presencas": presencas,
+    })
+
+
+def registrar_presenca_page(request, aluno_id):
+    aluno = get_object_or_404(Aluno, id=aluno_id)
+
+    if request.method == "POST":
+        config = ConfiguracaoDojo.objects.first()
+        duracao = config.duracao_aula_minutos if config else 75
+
+        Presenca.objects.create(
+            aluno=aluno,
+            data_aula=now().date(),
+            duracao_minutos=duracao,
+        )
+        return redirect("aluno-dashboard", aluno_id=aluno.id)
+
+    return render(request, "core/registrar_presenca.html", {
+        "aluno": aluno
+    })
