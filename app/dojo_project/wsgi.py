b22b76604c3592ac54a3f59ed0602d6b2ef23300
Renato Costa Ribas <ribascr@gmail.com>
2026-01-08 22:12:59 -0300

    Add WSGI configuration for dojo_project

---
+++ app/dojo_project/wsgi.py
@@ -0,0 +1,12 @@
+"""
+WSGI config for dojo_project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+"""
+
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dojo_project.settings')
+
+application = get_wsgi_application()
