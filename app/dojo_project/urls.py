d3d97933410aba41c60255afbab0641a086a4287
ribascr <ribascr@gmail.com>
2026-01-14 17:07:05 -0300

    rep2

--- app/dojo_project/urls.py
+++ app/dojo_project/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from app.dojo_core import views_admin
 from dojo_core.views_admin import relatorios_home
 
 urlpatterns = [
     path("admin/relatorios/", relatorios_home, name="admin-relatorios"),
     path("admin/", admin.site.urls),
     path("", include("dojo_core.urls")),
+    path("admin/relatorios/ranking-assiduidade/", views_admin.ranking_assiduidade, name="ranking-assiduidade"),
 ]
