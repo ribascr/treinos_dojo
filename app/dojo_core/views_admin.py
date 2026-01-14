b9d6785a7b4ae53cdab5e36262ac020fd20d53c1
ribascr <ribascr@gmail.com>
2026-01-14 17:12:57 -0300

    rep4

--- app/dojo_core/views_admin.py
+++ app/dojo_core/views_admin.py
@@ -1,5 +1,6 @@
 from django.contrib.admin.views.decorators import staff_member_required
 from django.template.response import TemplateResponse
+from dojo_core import views_admin
 from django.contrib import admin
 
 @staff_member_required
