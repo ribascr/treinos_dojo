85e629a23b558b62ae2228133d9d91bd1d652555
ribascr <ribascr@gmail.com>
2026-01-14 17:10:57 -0300

    rep3

--- app/dojo_project/urls.py
+++ app/dojo_project/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from app.dojo_core import views_admin
+from dojo_core import views_admin
 from dojo_core.views_admin import relatorios_home
 
 urlpatterns = [
