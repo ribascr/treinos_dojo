72ff4a48618fcbe2941f6aff97ec3289860951ef
ribascr <ribascr@gmail.com>
2026-01-14 13:13:23 -0300

    static

--- app/dojo_project/settings.py
+++ app/dojo_project/settings.py
@@ -68,7 +68,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "dojo_core" / "static",
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
