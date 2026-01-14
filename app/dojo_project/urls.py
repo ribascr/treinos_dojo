db955c9a8758121fbd4e8dfd4c635c683e9858a0
ribascr <ribascr@gmail.com>
2026-01-14 17:18:25 -0300

    rep6

--- app/dojo_project/urls.py
+++ app/dojo_project/urls.py
@@ -5,7 +5,7 @@ from dojo_core.views_admin import relatorios_home
 
 urlpatterns = [
     path("admin/relatorios/", relatorios_home, name="admin-relatorios"),
-    path("admin/", admin.site.urls),
-    path("", include("dojo_core.urls")),
     path("admin/relatorios/ranking-assiduidade/", views_admin.ranking_assiduidade, name="ranking-assiduidade"),
+    path("", include("dojo_core.urls")),
+    path("admin/", admin.site.urls),   
 ]
