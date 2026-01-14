082b827a3a1c5666c2aeaa3e97a3a0447730d37a
ribascr <ribascr@gmail.com>
2026-01-14 03:15:20 -0300

    final

--- app/dojo_project/urls.py
+++ app/dojo_project/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from dojo_core.views_admin import relatorios_home
 
 urlpatterns = [
+    path("admin/relatorios/", relatorios_home, name="admin-relatorios"),
     path("admin/", admin.site.urls),
     path("", include("dojo_core.urls")),
 ]
