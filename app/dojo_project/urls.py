e52952d995f835f8e5c849d6fccd92110df529c5
ribascr <ribascr@gmail.com>
2026-01-19 22:41:20 -0300

    report5

--- app/dojo_project/urls.py
+++ app/dojo_project/urls.py
@@ -10,8 +10,6 @@ urlpatterns = [
     path("admin/relatorios/ranking-assiduidade/", views_admin.ranking_assiduidade, name="ranking-assiduidade"),
     path("relatorios/horas-treinadas/", views_admin.relatorio_horas_treinadas, name="relatorio-horas-treinadas"),
     path("relatorios/alunos-por-faixa/",views_admin.relatorio_alunos_por_faixa, name="relatorio-alunos-por-faixa"),
-),
-
     path("", include("dojo_core.urls")),
     path("admin/", admin.site.urls),   
     path('accounts/login/', MeuLoginView.as_view(), name='login'),
