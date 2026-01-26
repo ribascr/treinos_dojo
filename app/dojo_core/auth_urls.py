d0b5789c60c5aadf36069601cc5c47dc72d9e099
ribascr <ribascr@gmail.com>
2026-01-25 21:22:00 -0300

    passwd15

--- app/dojo_core/auth_urls.py
+++ app/dojo_core/auth_urls.py
@@ -2,12 +2,36 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path("login/", auth_views.LoginView.as_view(), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-    path("password_change/", auth_views.PasswordChangeView.as_view(), name="password_change"),
-    path("password_change/done/", auth_views.PasswordChangeDoneView.as_view(), name="password_change_done"),
-    path("password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
-    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="accounts/login.html"
+        ),
+        name="login"
+    ),
+
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(
+            template_name="accounts/logged_out.html",
+            next_page="/accounts/login/"
+        ),
+        name="logout"
+    ),
+
+    path(
+        "password_change/",
+        auth_views.PasswordChangeView.as_view(
+            template_name="accounts/password_change_form.html"
+        ),
+        name="password_change"
+    ),
+
+    path(
+        "password_change/done/",
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name="accounts/password_change_done.html"
+        ),
+        name="password_change_done"
+    ),
 ]
